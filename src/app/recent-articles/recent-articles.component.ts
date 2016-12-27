cf3e9732253d11c126fa3eb8f33e04006a7f4474
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-27 18:36:39 -0200

    Imported code from plunker

--- src/app/recent-articles/recent-articles.component.ts
+++ src/app/recent-articles/recent-articles.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-recent-articles',
-  templateUrl: './recent-articles.component.html',
-  styleUrls: ['./recent-articles.component.css']
+  selector: 'recent-articles',
+  templateUrl: './recent-articles.component.html'
 })
-export class RecentArticlesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class RecentArticlesComponent {
+  articles: Article[] = [
+    new Article("Title 1", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
+    new Article("Title 2", new Date(), "https://unsplash.it/400?image=10", "image"),
+    new Article("Title 3", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
+    new Article("Title 4", new Date(), "https://unsplash.it/400?image=20", "image"),
+    new Article("Title 5", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
+    new Article("Title 6", new Date(), "https://unsplash.it/400?image=30", "image")
+  ];
 }
