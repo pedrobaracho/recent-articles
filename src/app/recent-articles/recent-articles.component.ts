8698d3a2e64d67cef623569c88ca2a005aa1f20f
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-27 20:15:24 -0200

    Finished implementation

--- src/app/recent-articles/recent-articles.component.ts
+++ src/app/recent-articles/recent-articles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from '../article';
 
 @Component({
   selector: 'recent-articles',
@@ -9,8 +10,8 @@ export class RecentArticlesComponent {
     new Article("Title 1", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
     new Article("Title 2", new Date(), "https://unsplash.it/400?image=10", "image"),
     new Article("Title 3", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
-    new Article("Title 4", new Date(), "https://unsplash.it/400?image=20", "image"),
+    new Article("Title 4", new Date(), "https://unsplash.it/401?image=20", "image"),
     new Article("Title 5", new Date(), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula", "text"),
-    new Article("Title 6", new Date(), "https://unsplash.it/400?image=30", "image")
+    new Article("Title 6", new Date(), "https://unsplash.it/402?image=30", "image")
   ];
 }
